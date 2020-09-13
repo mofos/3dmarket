a0efed23af03037ce2d5aab61f9c96843ff7b98a
mofos <44880207+mofos@users.noreply.github.com>
2020-09-13 17:34:51 +0530

    test1

--- hlb_milano_-_k-bed/script.js
+++ hlb_milano_-_k-bed/script.js
@@ -11,3 +11,14 @@
 	});
 
 })(jQuery);
+
+
+  document.querySelector("#model-viewer").addEventListener('ar-status', (event) => {
+    if(event.detail.status === 'failed'){
+      const error = document.querySelector("#error");
+      error.classList.remove('hide');
+      error.addEventListener('transitionend',(event) => {
+        error.classList.add('hide');
+      });
+    }
+  });
