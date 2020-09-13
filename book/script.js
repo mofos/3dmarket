f22b103d5f399ede26ef7344ccfb7bcf56e99e0e
mofos <44880207+mofos@users.noreply.github.com>
2020-09-13 17:20:42 +0530

    jk

--- book/script.js
+++ book/script.js
@@ -11,3 +11,13 @@
 	});
 
 })(jQuery);
+
+document.querySelector("#model-viewer").addEventListener('ar-status', (event) => {
+    if(event.detail.status === 'failed'){
+      const error = document.querySelector("#error");
+      error.classList.remove('hide');
+      error.addEventListener('transitionend',(event) => {
+        error.classList.add('hide');
+      });
+    }
+  });
